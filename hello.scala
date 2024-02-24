252c97bdfac383adb1fa2484817ad7909928e030
Simon Parten <quafadas@gmail.com>
2024-02-24 21:35:08 +0100

    Interactivity

--- hello.scala
+++ hello.scala
@@ -2,14 +2,29 @@ package webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import com.raquo.laminar.api.L.{*, given}
 
 @main
 def main: Unit =
-    appendParagraph(document.body, "Hello Scala.js!")
+  renderOnDomContentLoaded(
+    dom.document.getElementById("app"),
+    interactiveApp
+  )
 
-def appendParagraph(targetNode: dom.Node, text: String): Unit =
-  val parNode = document.createElement("p")
-  parNode.textContent = text
-  targetNode.appendChild(parNode)
-
-
+def interactiveApp =
+  val hiVar = Var("World!")
+  div(
+    h1(
+      "Hello ",
+      child.text <-- hiVar.signal
+    ),
+    p("This is a simple example of a Laminar app."),
+    // https://demo.laminar.dev/app/form/controlled-inputs
+    input(
+      typ := "text",
+      controlled(
+        value <-- hiVar.signal,
+        onInput.mapToValue --> hiVar.writer
+      )
+    )
+  )
--- project.scala
+++ project.scala
@@ -1,6 +1,8 @@
+//> using scala 3.4.0
 //> using platform js
 
 //> using dep org.scala-js::scalajs-dom::2.8.0
+//> using dep com.raquo::laminar::17.0.0-M6
 
-//> using scala 3.4.0
+
 
