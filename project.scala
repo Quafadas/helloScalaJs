e56327cb5f08c0ce12bed3a14165bc2b72845fea
Simon Parten <quafadas@gmail.com>
2024-02-25 09:34:17 +0100

    ES modules

--- project.scala
+++ project.scala
@@ -4,5 +4,7 @@
 //> using dep org.scala-js::scalajs-dom::2.8.0
 //> using dep com.raquo::laminar::17.0.0-M6
 
-
+//> using jsModuleKind es
+//> using jsModuleSplitStyleStr smallmodulesfor
+//> using jsSmallModuleForPackage webapp
 
